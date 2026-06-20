acb59378d691fa2e3a2c80e21e0d9fc16fbb904a
Muhammad Hani Ilma Alfarabi <Ladenturnover12@gmail.com>
2026-06-20 09:42:54 +0800

    Fixing Web Load Problem

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,20 +4,20 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781919134|60185456';
+const CACHE_VERSION = '1781919700|29491450';
 /** @type {string} */
 const CACHE_PREFIX = 'Echoes Of Ashes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
-const OFFLINE_URL = 'EchoesOfAshes-v0.6 Beta 1.offline.html';
+const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["EchoesOfAshes-v0.6 Beta 1.html","EchoesOfAshes-v0.6 Beta 1.js","EchoesOfAshes-v0.6 Beta 1.offline.html","EchoesOfAshes-v0.6 Beta 1.icon.png","EchoesOfAshes-v0.6 Beta 1.apple-touch-icon.png","EchoesOfAshes-v0.6 Beta 1.audio.worklet.js","EchoesOfAshes-v0.6 Beta 1.audio.position.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["EchoesOfAshes-v0.6 Beta 1.wasm","EchoesOfAshes-v0.6 Beta 1.pck"];
+const CACHEABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
